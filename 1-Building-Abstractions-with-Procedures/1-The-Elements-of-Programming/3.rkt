fb34e38632c176236cb3183f56ef2adda485fbe6
loseway216 <loseway216@gmail.com>
2022-11-03 18:12:25 +0800

    chapter 1

--- 1-Building-Abstractions-with-Procedures/1-The-Elements-of-Programming/3.rkt
+++ 1-Building-Abstractions-with-Procedures/1-The-Elements-of-Programming/3.rkt
@@ -1,6 +1,5 @@
 #lang sicp
-; Define a procedure that takes three numbers as arguments
-; and returns the sum of the squares of the two larger numbers.
+; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 (define (square x)
   (* x x))
 
