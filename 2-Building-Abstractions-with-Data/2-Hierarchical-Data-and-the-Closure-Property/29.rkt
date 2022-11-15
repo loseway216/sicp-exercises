bf007b5dbb54a28596b537696ee5c82d3fe55f6c
loseway216 <loseway216@gmail.com>
2022-11-15 23:11:00 +0800

    chap2

--- 2-Building-Abstractions-with-Data/2-Hierarchical-Data-and-the-Closure-Property/29.rkt
+++ 2-Building-Abstractions-with-Data/2-Hierarchical-Data-and-the-Closure-Property/29.rkt
@@ -22,12 +22,16 @@
   (cond
     [(null? mobile) 0]
     [(not (pair? mobile)) mobile]
-    [else
-     (+ (total-weight (branch-structure (left-branch mobile)))
-        (total-weight (branch-structure (right-branch mobile))))]))
+    [else (+ (total-weight (branch-structure (left-branch mobile)))
+             (total-weight (branch-structure (right-branch mobile))))]))
 
+(define (balanced mobile)
+  (let ([top-left-branch (left-branch mobile)] [top-right-branch (right-branch mobile)])
+    (= (* (branch-length top-left-branch) (total-weight (branch-structure top-left-branch)))
+       (* (branch-length top-right-branch) (total-weight (branch-structure top-right-branch))))))
 ; test
 (define m
-  (make-mobile (make-branch 1 2) (make-branch 3 (make-mobile (make-branch 1 3) (make-branch 3 4)))))
+  (make-mobile (make-branch 3 7) (make-branch 3 (make-mobile (make-branch 1 3) (make-branch 3 4)))))
 
 (total-weight m)
+(balanced m)
